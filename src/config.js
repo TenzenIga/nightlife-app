76fe60eadcb6be6e73bdfe5735b2b6c964d80f14
Zeta <arhan-93@list.ru>
2018-05-05 21:15:32 +0300

    Delete config.js

--- src/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  database:process.env.ML_URI,
-  secret: 'secret',
-  googleKey:'AIzaSyBNsmAqXHlbvZ_zz0SofURkOX1AVJQ6iY0',
-  consumerKey: 'rLAFm8CVNCmYMPgQ9qGC52ML1',
-  consumerSecret: 'xSzATFycQhj4qldKy7sDB1oB477xeCATz6BFJhHsAaZy6X9YDU'
-}
